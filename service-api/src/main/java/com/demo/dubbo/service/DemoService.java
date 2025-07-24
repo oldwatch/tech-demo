51abed78570960ef7af40cfbd8c991a8274c76cc
java-jiang <oldwatch2005@gmail.com>
2025-07-24 16:34:56 +0800

    move dubbo config to nacos

--- service-api/src/main/java/com/demo/dubbo/service/DemoService.java
+++ service-api/src/main/java/com/demo/dubbo/service/DemoService.java
@@ -3,17 +3,26 @@ package com.demo.dubbo.service;
 import com.demo.dubbo.service.entity.DemoEntity;
 import com.demo.dubbo.service.entity.DemoInput;
 import com.demo.dubbo.service.entity.Pager;
+import org.apache.dubbo.remoting.http12.HttpMethods;
+import org.apache.dubbo.remoting.http12.rest.Mapping;
+import org.apache.dubbo.remoting.http12.rest.Param;
+import org.apache.dubbo.remoting.http12.rest.ParamType;
 
 import java.util.List;
 
 public interface DemoService {
 
+    @Mapping(enabled = false)
     DemoEntity addData(DemoInput input);
 
+
+    @Mapping(path = "/all", method = HttpMethods.GET)
     List<DemoEntity> getDataListByPage(Pager pager);
 
+    @Mapping(enabled = false)
     DemoEntity updateData(String id, DemoInput input);
 
-    DemoEntity queryByID(String id);
+    @Mapping(path = "/id/{id}", method = HttpMethods.GET)
+    DemoEntity queryByID(@Param(type = ParamType.PathVariable) String id);
 
 }
